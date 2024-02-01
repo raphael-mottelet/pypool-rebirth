058fbbb4e3493efb65c729badab04296c91f68e9
raphael-mottelet <72501205+raphael-mottelet@users.noreply.github.com>
2024-02-01 14:39:43 +0100

    adding further comments about the empty gamecamera.py file

--- game_character/gamecamera.py
+++ game_character/gamecamera.py
@@ -1,1 +1,4 @@
 #code non fonctionnel, cette partie n'est pas push.
+
+#Quel était l'idée de la caméra ? : Avoir un buffer en pixel ou si le joueur se raproche trop près de la bordure de la fenettre, la fenettre suit la direction du joueur sur la map
+#tout en permettant au joueur de voir ce qui arrive devant lui grace au buffer (cette méthode aurait aussi pu servir à détecter les collisions avec la camera mais mon approche aura été différente.)
